206ca9b4f34f24f2126e3cf11125e2fe6b94c122
Sahej Singh <sahejdeep@hotmail.com>
2020-05-08 04:33:44 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -22,13 +22,13 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-
+  var name = "Cricky";
   var name = "Kristin";
 
 
   function sayMy() {
     console.log(name);
-
+  sayMy();
   }
-  sayMyName();
+  sayMyName()()
 }
